5605696a531ff14e1c2b44e92a426983c095f151
SakaBurak <bekirburaksaka@gmail.com>
2023-11-11 15:38:50 +0300

    division

---
+++ bolumkalan.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int sayi1 , sayi2 , bolum , kalan;
+	
+	cout<<"Lutfen bolmek istediginiz sayilari giriniz!"<<endl;
+	cout<<"1.sayi:";
+	cin>>sayi1;
+	cout<<"2.sayi:";
+	cin>>sayi2;
+	
+	bolum=sayi1/sayi2;
+	kalan=sayi1%sayi2;
+	
+	cout<<"Girmis oldugunuz "<<sayi1<<" sayisinin "<<sayi2<<" sayisina bolumu:"<<bolum<<" kalan:"<<kalan<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	
+	cout<<"Bekir Burak SAKA"<<endl;
+	cout<<"221030910008";
+	
+	return 0; 
+}
